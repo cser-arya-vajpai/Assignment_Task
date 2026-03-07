1578f93daf130da2112d9181302d0fa0752be77f
cser-arya-vajpai <cser.arya.vajpai@gmail.com>
2026-03-07 12:41:12 +0530

    Update JavaApp.java
    Implemented thread creation using Runnable interface also.

--- multi_threading/JavaApp.java
+++ multi_threading/JavaApp.java
@@ -1,18 +1,31 @@
 package multi_threading;
 
+// JavaApp class used to execute and start multiple threads
 public class JavaApp {
 
     public static void execution() {
         
         // Creating an object of MyThread1 class
-        // This object represents a new thread
+        // MyThread1 extends the Thread class
+        // So this thread is created using the "Thread Class Inheritance" method
         MyThread1 myThread1 = new MyThread1();
         
-        // Calling start() to begin thread execution
+        // Calling start() to begin execution of the thread
         // start() creates a new thread and internally calls the run() method
-        // If we call run() directly, it will execute like a normal method
-        // and no new thread will be created (no multi-threading)
+        // If run() were called directly, it would behave like a normal method
+        // and no new thread would be created (no multithreading)
         myThread1.start();
         
+        
+        // Creating an object of MyThread2 class
+        // MyThread2 implements the Runnable interface
+        MyThread2 myThread2 = new MyThread2();
+        
+        // Creating a Thread object and passing the Runnable object to it
+        // This is required because Runnable cannot start a thread by itself
+        Thread thread = new Thread(myThread2);
+        
+        // Starting the thread which executes the run() method defined in MyThread2
+        thread.start();
     }
 }
